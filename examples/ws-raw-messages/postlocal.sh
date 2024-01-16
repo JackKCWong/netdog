4e3d4ac6a0c22fc632311a37ec18f369b39c4ca3
JackKCWong <jiajue.huang@live.com>
2024-01-16 22:21:01 +0800

    add ws raw message examples

---
+++ examples/ws-raw-messages/postlocal.sh
@@ -0,0 +1,2 @@
+go-netdog localhost:12345 ws-local.http <(xxd -r <f1.txt) <(xxd -r <f2.txt) close.bin
+
---
+++ examples/ws-raw-messages/postman.sh
@@ -0,0 +1,3 @@
+
+go-netdog --tls ws.postman-echo.com:443 ws-handshake.http <(xxd -r <f1.txt) <(xxd -r <f2.txt) close.bin
+
